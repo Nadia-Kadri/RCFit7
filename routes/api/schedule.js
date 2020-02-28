6e2a30b5c97476236b724062482b7bcedf6e7ec7
Nadia-Kadri <nadiagrace96@gmail.com>
2020-02-28 09:15:00 -0500

    sign up for schedule, admin view schedule

--- routes/api/schedule.js
+++ routes/api/schedule.js
@@ -8,8 +8,14 @@ router.post("/create", function(req, res) {
 
   db.Schedule.create({ datetime: req.body.datetime, class: req.body.class, trainer: req.body.trainer })
   .then(result => {
-    console.log(result)
-    res.send(result)
+    db.Schedule.findOne({ _id: result._id })
+    .populate("class")
+    .populate("trainer")
+    .then(popResult => {
+      console.log(popResult)
+      res.send(popResult)
+    })
+    .catch(err => console.log(err.message));
   })
   .catch(err => console.log(err.message));
 });
@@ -26,6 +32,27 @@ router.get("/userView", function(req, res) {
     res.send(result)
   })
   .catch(err => console.log(err.message));
+})
+
+// Put route for users to sign up for classes
+router.put("/user/signup/:classId/:userId", function(req, res) {
+  db.Schedule.findOneAndUpdate({ _id: req.params.classId }, { $push: { users: req.params.userId } }, { new: true })
+    .then(result => {
+      console.log(result)
+      res.json(result)
+    })
+    .catch(err => console.log(err.message));
+});
+
+// Get route for Admin to see who is signed up for a specific classes
+router.get("/adminView/:id", function(req, res) {
+  db.Schedule.findOne({ _id: req.params.id })
+    .populate("users")
+    .then(result => {
+      console.log(result)
+      res.send(result)
+    })
+    .catch(err => console.log(err.message));
 })
 
 module.exports = router;
