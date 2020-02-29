02a74694efcc4a10bb706709989e779d6e359026
Nadia-Kadri <nadiagrace96@gmail.com>
2020-02-29 18:40:47 -0500

    schedule page

--- client/src/pages/Schedule/Schedule.js
+++ client/src/pages/Schedule/Schedule.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import API from "../../utils/scheduleAPI";
 import UpcomingClasses from "./UpcomingClasses";
+import ScheduleCalendar from "./ScheduleCalendar";
+import "./index.css";
 
 class Schedule extends Component {
   state = {
@@ -24,7 +26,16 @@ class Schedule extends Component {
     return (
       <React.Fragment>
         <h1>Schedule Page</h1>
-        <UpcomingClasses />
+        <div className="custContainer">
+          <div className="row">
+            <div className="col-sm-5">
+              <UpcomingClasses />
+            </div>
+            <div className="col-sm-7">
+              <ScheduleCalendar />
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
