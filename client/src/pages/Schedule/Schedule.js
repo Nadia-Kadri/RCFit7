bfb8cd183467dbf6b714172b033f441e5a7159a0
Nadia-Kadri <nadiagrace96@gmail.com>
2020-02-29 21:51:40 -0500

    upcoming schedule

--- client/src/pages/Schedule/Schedule.js
+++ client/src/pages/Schedule/Schedule.js
@@ -1,27 +1,10 @@
 import React, { Component } from "react";
-import API from "../../utils/scheduleAPI";
 import UpcomingClasses from "./UpcomingClasses";
 import ScheduleCalendar from "./ScheduleCalendar";
 import "./index.css";
 
 class Schedule extends Component {
-  // state = {
-  //   events: []
-  // };
-
-  // componentDidMount() {
-  //   this.getEvents()
-  // };
-
-  // getEvents = () => {
-  //   API.userSchedule()
-  //     .then(res => { 
-  //       this.setState({ events: res.data })
-  //       // console.log(this.state.events)
-  //     })
-  //     .catch(err => console.log(err))
-  // }
-
+  
   render() {
     return (
       <React.Fragment>
--- client/src/pages/Schedule/UpcomingClasses.js
+++ client/src/pages/Schedule/UpcomingClasses.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
+import API from "../../utils/scheduleAPI";
 import UpcomingClass from "./UpcomingClass";
 
 class UpcomingClasses extends Component {
 
+  state = {
+    events: []
+  };
+
+  componentDidMount() {
+    this.getEvents()
+  };
+
+  getEvents = () => {
+    API.userSchedule()
+      .then(res => { 
+        this.setState({ events: res.data })
+      })
+      .catch(err => console.log(err))
+  }
+
+  userSignUp = (userID, classID) => {
+
+  }
+
+  onClick = (id) => {
+    console.log(id)
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <UpcomingClass />
-        <UpcomingClass />
-        <UpcomingClass />
-      </React.Fragment>
+      this.state.events.map(event => (
+        <UpcomingClass key={event._id} id={event._id} title={event.class.title} duration={event.class.duration} trainer={event.trainer.firstName} onClick={this.onClick}/>
+      ))
     );
   }
 }
