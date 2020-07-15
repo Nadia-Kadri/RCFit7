9526999c0609df57397c2fae17c47e1abb9f382f
Maserall Marcelin <maserallm@knights.ucf.edu>
2020-07-15 15:38:45 -0400

    added mobile styles to app

--- client/src/pages/Schedule/Schedule.js
+++ client/src/pages/Schedule/Schedule.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import API from "../../utils/scheduleAPI";
-import moment from "moment"
+import moment from "moment";
 import UpcomingSchedules from "./UpcomingSchedules";
 import ScheduleCalendar from "./ScheduleCalendar";
 import LoginModal from "./Modals/LoginModal";
@@ -10,23 +10,24 @@ import image from "../../images/schedule2.jpg";
 import "./index.css";
 
 class Schedule extends Component {
-
   state = {
     schedules: []
   };
 
   componentDidMount() {
-    this.getSchedules()
-  };
+    this.getSchedules();
+  }
 
   getSchedules = () => {
     API.userSchedule()
-      .then(res => { 
-        let schedulesArr = []
-        for(let i = 0; i < res.data.length; i++) {
-          let isSignedUp
-          let found = res.data[i].users.some(user => user === this.props.userId)
-          found ? isSignedUp = true : isSignedUp = false
+      .then(res => {
+        let schedulesArr = [];
+        for (let i = 0; i < res.data.length; i++) {
+          let isSignedUp;
+          let found = res.data[i].users.some(
+            user => user === this.props.userId
+          );
+          found ? (isSignedUp = true) : (isSignedUp = false);
 
           schedulesArr.push({
             id: res.data[i]._id,
@@ -34,34 +35,36 @@ class Schedule extends Component {
             duration: res.data[i].class.duration,
             trainer: res.data[i].trainer.firstName,
             datetime: new Date(res.data[i].datetime),
-            users: res.data[i].users, 
+            users: res.data[i].users,
             isSignedUp: isSignedUp
-          })
+          });
         }
-        const signUpSchedulesArr = schedulesArr.filter(e => {return e.datetime < moment().add(7, 'days')})
-        this.setState({ schedules: signUpSchedulesArr })
+        const signUpSchedulesArr = schedulesArr.filter(e => {
+          return e.datetime < moment().add(7, "days");
+        });
+        this.setState({ schedules: signUpSchedulesArr });
       })
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
 
-  userSignUp = (scheduleId) => {
+  userSignUp = scheduleId => {
     API.userSignUp(scheduleId)
       .then(res => {
-        console.log(res)
-        this.getSchedules()
+        console.log(res);
+        this.getSchedules();
       })
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
 
-  userCancel = (scheduleId) => {
+  userCancel = scheduleId => {
     API.userCancel(scheduleId)
       .then(res => {
-        console.log(res)
-        this.getSchedules()
+        console.log(res);
+        this.getSchedules();
       })
-      .catch(err => console.log(err))
-  }
-  
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -73,23 +76,26 @@ class Schedule extends Component {
           <div className="container h-100">
             <div className="row h-100">
               <div className="col-12" id="masthead-text-margin">
-                <h1 className="float-right">SCHEDULE <i className="fa fa-angle-double-down" aria-hidden="true"></i></h1>
+                <h1>
+                  SCHEDULE{" "}
+                  <i className="fa fa-angle-double-down" aria-hidden="true"></i>
+                </h1>
               </div>
             </div>
-          </div>  
+          </div>
         </div>
 
-
         <div className="container-fluid">
-
           <div className="row">
-
             <div className="col-sm-7">
               <div className="card bg-light mb-3 custCard upcomingLeft">
                 <div className="card-body">
                   <h4 className="card-title">Upcoming Classes</h4>
                   <div className="card-text">
-                    <h6>Below are this week's upcoming classes, click Sign-Up to reserve your spot!</h6>
+                    <h6>
+                      Below are this week's upcoming classes, click Sign-Up to
+                      reserve your spot!
+                    </h6>
                     <table className="table table-hover table-sm">
                       <thead>
                         <tr>
@@ -101,7 +107,12 @@ class Schedule extends Component {
                         </tr>
                       </thead>
                       <tbody>
-                        <UpcomingSchedules userId={this.props.userId} schedules={this.state.schedules} userSignUp={this.userSignUp} userCancel={this.userCancel} />
+                        <UpcomingSchedules
+                          userId={this.props.userId}
+                          schedules={this.state.schedules}
+                          userSignUp={this.userSignUp}
+                          userCancel={this.userCancel}
+                        />
                       </tbody>
                     </table>
                   </div>
@@ -109,8 +120,8 @@ class Schedule extends Component {
               </div>
             </div>
 
-            <img src={image} className="col-sm-5 mb-3"/>
-          </div> 
+            <img src={image} className="col-sm-5 mb-3" />
+          </div>
 
           <div className="row">
             <div className="col-sm-12">
@@ -119,8 +130,8 @@ class Schedule extends Component {
                   <h4 className="card-title">Monthly Schedule</h4>
                   <div className="card-text">
                     <h6>Like to plan ahead? View the entire schedule below!</h6>
-                    <div className="ml-5 mr-5">
-                      <div className="m-5">
+                    <div className=" cal_spacing">
+                      <div className=" cal_spacing">
                         <ScheduleCalendar />
                       </div>
                     </div>
@@ -129,7 +140,6 @@ class Schedule extends Component {
               </div>
             </div>
           </div>
-
         </div>
         <LoginModal />
         <SignUpModal getSchedules={this.getSchedules} />
