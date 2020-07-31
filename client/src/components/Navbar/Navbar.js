e39295af847946966352b0b35ff9c128097d1725
Maserall Marcelin <maserallm@knights.ucf.edu>
2020-07-30 21:43:42 -0400

    Revert "added mobile styles to app"
    This reverts commit 9526999c0609df57397c2fae17c47e1abb9f382f.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -8,27 +8,18 @@ class Navbar extends React.Component {
     return (
       <React.Fragment>
         <nav className="navbar navbar-expand-lg navbar-light bg-light index cust-nav">
+
           <Link to="/" className="nav-link">
             <SvgComponent style={{ height: "60px", width: "60px" }} />
           </Link>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div
-            className="collapse navbar-collapse ham_menu"
-            id="navbarNavAltMarkup"
-          >
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav ml-auto">
+              
               <Link to="/" className="nav-link border-right">
                 <span className="float-right text">
                   Home <span className="sr-only">(current)</span>
@@ -53,42 +44,40 @@ class Navbar extends React.Component {
                 </span>
               </Link>
 
-              {this.props.authorized && !this.props.isAdmin ? (
-                <Link to="/user/profile" className="nav-link border-right">
-                  <span className="float-right text">
-                    User Profile <span className="sr-only">(current)</span>
-                  </span>
-                </Link>
-              ) : (
-                ""
-              )}
+            {this.props.authorized && !this.props.isAdmin ? (
+              <Link to="/user/profile" className="nav-link border-right">
+                <span className="float-right text">
+                  User Profile <span className="sr-only">(current)</span>
+                </span>
+              </Link>
+            ) : (
+              ""
+            )}
 
-              {this.props.authorized && this.props.isAdmin ? (
-                <Link to="/admin" className="nav-link border-right">
-                  <span className="float-right text">
-                    Admin Page <span className="sr-only">(current)</span>
-                  </span>
-                </Link>
-              ) : (
-                ""
-              )}
+            {this.props.authorized && this.props.isAdmin ? (
+              <Link to="/admin" className="nav-link border-right">
+                <span className="float-right text">
+                  Admin Page <span className="sr-only">(current)</span>
+                </span>
+              </Link>
+            ) : (
+              ""
+            )}
 
-              {this.props.authorized ? (
-                <Link to="/" className="nav-link">
-                  <span
-                    className="float-right text"
-                    onClick={this.props.logout}
-                  >
-                    Logout <span className="sr-only">(current)</span>
-                  </span>
-                </Link>
-              ) : (
-                <Link to="/login" className="nav-link">
-                  <span className="float-right text">
-                    Login / Sign-Up <span className="sr-only">(current)</span>
-                  </span>
-                </Link>
-              )}
+            {this.props.authorized ? (
+              <Link to="/" className="nav-link">
+                <span className="float-right text" onClick={this.props.logout}>
+                  Logout <span className="sr-only">(current)</span>
+                </span>
+              </Link>
+            ) : (
+              <Link to="/login" className="nav-link">
+                <span className="float-right text">
+                  Login / Sign-Up <span className="sr-only">(current)</span>
+                </span>
+              </Link>
+            )}
+
             </div>
           </div>
         </nav>
